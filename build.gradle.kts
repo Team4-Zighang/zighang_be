ee91407bf95641e6d990a65d2a261f7154e5448c
김영록 <praoo800@konkuk.ac.kr>
2025-08-21 17:42:17 +0900

    feat : DB를 연동한다. #11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "2.1.0"
     id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -44,6 +45,12 @@ dependencies {
     // redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis:3.3.10")
     implementation("org.redisson:redisson:3.44.0")
+
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // mysql
+    runtimeOnly("mysql:mysql-connector-java:8.0.33")
 
 
     implementation("org.springframework.boot:spring-boot-starter-web")
