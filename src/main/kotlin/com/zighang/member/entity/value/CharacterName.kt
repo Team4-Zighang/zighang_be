cd361c8784e7e2098c5de89a6d2fe34fce5f995e
김영록 <praoo800@konkuk.ac.kr>
2025-08-24 15:47:33 +0900

    feat : 온보딩 dto를 정의한다. #16

---
+++ src/main/kotlin/com/zighang/member/entity/value/CharacterName.kt
@@ -0,0 +1,14 @@
+package com.zighang.member.entity.value
+
+enum class CharacterName(
+    val displayName: String,
+) {
+    DUMJIK_HAENG("듬직행"),
+    SILSOK_HAENG("실속행"),
+    SEONGSIL_HAENG("성실행"),
+    DANJJAN_HAENG("단짠행"),
+    MOHEOM_HAENG("모험행"),
+    JJIN_DONG_HAENG("찐동행"),
+    DOJEON_HAENG("도전행"),
+    JAYU_HAENG("자유행")
+}
---
+++ src/main/kotlin/com/zighang/member/entity/value/CompanyType.kt
@@ -0,0 +1,8 @@
+package com.zighang.member.entity.value
+
+enum class CompanyType(
+    val displayName: String
+) {
+    STABLE("안정형"),
+    CHALLENGING("도전형")
+}
