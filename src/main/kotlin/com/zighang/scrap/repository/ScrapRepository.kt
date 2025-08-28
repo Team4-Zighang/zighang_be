6c735467530163ea046641f98f262030639c901e
김영록 <praoo800@konkuk.ac.kr>
2025-08-29 00:51:38 +0900

    refactor : 페이징 지원 로직 추가 #32

--- src/main/kotlin/com/zighang/scrap/repository/ScrapRepository.kt
+++ src/main/kotlin/com/zighang/scrap/repository/ScrapRepository.kt
@@ -1,8 +1,10 @@
 package com.zighang.scrap.repository
 
 import com.zighang.scrap.entity.Scrap
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ScrapRepository : JpaRepository<Scrap, Long> {
-    fun findAllByMemberId(memberId : Long) : List<Scrap>
+    fun findAllByMemberId(memberId : Long, pageable: Pageable) : Page<Scrap>
 }
