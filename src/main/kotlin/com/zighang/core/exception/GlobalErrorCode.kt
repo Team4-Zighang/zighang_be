90bd8ec3f16dfff1e8fd1c19845c39b76168faa8
김영록 <praoo800@konkuk.ac.kr>
2025-08-29 01:10:01 +0900

    chore : 문법 오류를 고친다. #32

--- src/main/kotlin/com/zighang/core/exception/GlobalErrorCode.kt
+++ src/main/kotlin/com/zighang/core/exception/GlobalErrorCode.kt
@@ -16,7 +16,7 @@ enum class GlobalErrorCode(
     FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
     FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제에 실패했습니다."),
     NOT_EXIST_SCRAP(HttpStatus.BAD_REQUEST, "해당 스크랩 공고가 존재하지 않습니다."),
-    NOT_EXIST_JOB_POSTING(HttpStatus.BAD_REQUEST, "해당 공고를 찾을 수 없습니다.");
+    NOT_EXIST_JOB_POSTING(HttpStatus.BAD_REQUEST, "해당 공고를 찾을 수 없습니다."),
     CLOVA_API_CALL_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "클로바 API 연결에 실패했습니다."),
     NOT_CONVERT_JSON_TO_OBJECT(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 변환에 실패했습니다.");
 
