7225c4603f3710eeda94ffbd40a9f1adebeac1ee
chyun7114 <chyun7114@naver.com>
2025-08-29 16:23:45 +0900

    Chore: 로그 삭제 및 서브모듈 커밋 반영

--- src/main/kotlin/com/zighang/jobposting/repository/JobPostingRepository.kt
+++ src/main/kotlin/com/zighang/jobposting/repository/JobPostingRepository.kt
@@ -1,9 +1,17 @@
 package com.zighang.jobposting.repository
 
 import com.zighang.jobposting.entity.JobPosting
+import jakarta.transaction.Transactional
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface JobPostingRepository : CrudRepository<JobPosting, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE JobPosting j SET j.qualification = :qualification, j.preferentialTreatment = :preferentialTreatment WHERE j.id = :id")
+    fun updateJobPostingAnalysis(id: Long, qualification: String, preferentialTreatment: String)
 }
--- src/main/kotlin/com/zighang/scrap/infrastructure/worker/AIRequestWorker.kt
+++ src/main/kotlin/com/zighang/scrap/infrastructure/worker/AIRequestWorker.kt
@@ -1,36 +1,29 @@
 package com.zighang.scrap.infrastructure.worker
 
 import com.zighang.core.clova.util.JsonCleaner
-import com.zighang.core.config.rabbitmq.config.RabbitProperties
+import com.zighang.jobposting.repository.JobPostingRepository
 import com.zighang.scrap.dto.request.JobEnrichedEvent
 import com.zighang.scrap.dto.request.JobScrapedEvent
 import com.zighang.scrap.dto.response.JobPostingAnalysisDto
 import com.zighang.scrap.infrastructure.JobAnalysisCaller
 import com.zighang.scrap.infrastructure.JobAnalysisEventProducer
 import com.zighang.scrap.infrastructure.mapper.JobAnalysisDtoMapper
-import lombok.extern.slf4j.Slf4j
-import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 
 @Component
-@Slf4j
-class AIReqeustWorker(
+class AIRequestWorker(
     private val jobAnalysisEventProducer: JobAnalysisEventProducer,
     private val jobAnalysisCaller: JobAnalysisCaller,
-    private val jobAnalysisDtoMapper: JobAnalysisDtoMapper
+    private val jobAnalysisDtoMapper: JobAnalysisDtoMapper,
+    private val jobPostingRepository: JobPostingRepository
 ) {
-
-    private val log = LoggerFactory.getLogger(AIReqeustWorker::class.java)
-
     @RabbitListener(queues= ["\${mq.analysis.name}"])
     fun jobPostingToClova(event : JobScrapedEvent) {
-        // clova 자격 우대사항 요건
+        // clova 자격요건/우대사항 분석
         val result = jobAnalysisCaller.call(event.ocrData).result.message.content
 
         val jobPostingAnalysisDto = jobAnalysisDtoMapper.toJsonDto(JsonCleaner.cleanJson(result))
-
-        log.info(jobPostingAnalysisDto.toString())
 
         jobAnalysisEventProducer.publishEnriched(
             JobEnrichedEvent(
@@ -40,13 +33,14 @@ class AIReqeustWorker(
                 )
             )
         )
-        log.info("publish : analysis -> enrich")
     }
 
     @RabbitListener(queues= ["\${mq.enriched.name}"])
     fun jobEnriched(event : JobEnrichedEvent) {
-        log.info("enrich consumer : $event")
-
-        // DB 업데이트 로직
+        jobPostingRepository.updateJobPostingAnalysis(
+            event.id,
+            event.jobPostingAnalysisDto.qualification,
+            event.jobPostingAnalysisDto.preferentialTreatment
+        )
     }
 }
