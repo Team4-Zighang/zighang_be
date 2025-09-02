132c723a25ee94bbf00c80d448ec31c29694cd81
chyun7114 <chyun7114@naver.com>
2025-09-02 23:34:30 +0900

    Chore: 학교 영문명 오타 수정

--- src/main/kotlin/com/zighang/member/entity/value/School.kt
+++ src/main/kotlin/com/zighang/member/entity/value/School.kt
@@ -7,35 +7,35 @@ enum class School(
     YEONSEI("연세대학교"),
     KOREA("고려대학교"),
 
-    SEOKANG("서강대학교"),
-    SEONGKYUN("성균관대학교"),
+    SOGANG("서강대학교"),
+    SUNGKYUNKWAN("성균관대학교"),
     HANYANG("한양대학교"),
-    EHWA("이화여자대학교"),
+    EWHA("이화여자대학교"),
 
     CHUNGANG("중앙대학교"),
-    KYEONGHUI("경희대학교"),
+    KYUNGHEE("경희대학교"),
     HUFS("한국외국어대학교"),
-    SEOUL_CITY("서울시립대학교"),
+    UNIVERSITY_OF_SEOUL("서울시립대학교"),
 
-    KUNKOOK("건국대학교"),
-    DONGKUK("동국대학교"),
+    KONKUK("건국대학교"),
+    DONGGUK("동국대학교"),
     HONGIK("홍익대학교"),
-    SOOKMYEONG("숙명여대"),
+    SOOKMYUNG("숙명여자대학교"),
 
     KOOKMIN("국민대학교"),
     SOONGSIL("숭실대학교"),
     SEJONG("세종대학교"),
-    DANKGUK("단국대학교"),
+    DANKOOK("단국대학교"),
     HANYANG_ERICA("한양대학교 에리카캠퍼스"),
 
     KWANGWOON("광운대학교"),
     MYEONGJI("명지대학교"),
-    SNAGMYEONG("상명대학교"),
+    SANGMYUNG("상명대학교"),
     CATHOLIC("가톨릭대학교"),
     SUNGSHIN("성신여자대학교"),
 
     INCHEON("인천대학교"),
-    GACHEON("가천대학교"),
+    GACHON("가천대학교"),
     KYONGGI("경기대학교")
     ;
 
