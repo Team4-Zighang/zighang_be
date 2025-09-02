cee6dfc28cdda4d532f3eaf0e68151b991d5419b
chyun7114 <chyun7114@naver.com>
2025-09-02 21:01:53 +0900

    Feat: School enum 설정

---
+++ src/main/kotlin/com/zighang/member/entity/value/School.kt
@@ -0,0 +1,41 @@
+package com.zighang.member.entity.value
+
+enum class School(
+    val schoolName: String,
+) {
+    SEOUL("서울대학교"),
+    YEONSEI("연세대학교"),
+    KOREA("고려대학교"),
+
+    SEOKANG("서강대학교"),
+    SEONGKYUN("성균관대학교"),
+    HANYANG("한양대학교"),
+    EHWA("이화여자대학교"),
+
+    CHUNGANG("중앙대학교"),
+    KYEONGHUI("경희대학교"),
+    HUFS("한국외국어대학교"),
+    SEOUL_CITY("서울시립대학교"),
+
+    KUNKOOK("건국대학교"),
+    DONGKUK("동국대학교"),
+    HONGIK("홍익대학교"),
+    SOOKMYEONG("숙명여대"),
+
+    KOOKMIN("국민대학교"),
+    SOONGSIL("숭실대학교"),
+    SEJONG("세종대학교"),
+    DANKGUK("단국대학교"),
+    HANYANG_ERICA("한양대학교 에리카캠퍼스"),
+
+    KWANGWOON("광운대학교"),
+    MYEONGJI("명지대학교"),
+    SNAGMYEONG("상명대학교"),
+    CATHOLIC("가톨릭대학교"),
+    SUNGSHIN("성신여자대학교"),
+
+    INCHEON("인천대학교"),
+    GACHEON("가천대학교"),
+    KYONGGI("경기대학교")
+    ;
+}
