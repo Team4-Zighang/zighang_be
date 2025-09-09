65e770d8ca670b865cc345fc0628ec71a37bd58c
chyun7114 <chyun7114@naver.com>
2025-09-09 22:07:44 +0900

    Feat: 1차 공고 dto 변수 설정 완료

--- src/main/kotlin/com/zighang/jobposting/service/JobPostingService.kt
+++ src/main/kotlin/com/zighang/jobposting/service/JobPostingService.kt
@@ -6,7 +6,13 @@ import com.zighang.card.service.CardService
 import com.zighang.card.value.CardPosition
 import com.zighang.jobposting.repository.JobPostingRepository
 import com.zighang.jobposting.dto.event.JobAnalysisEvent
+import com.zighang.jobposting.dto.response.JobPostingDetailResponseDto
+import com.zighang.jobposting.entity.JobPosting
+import com.zighang.jobposting.entity.value.RecruitmentType
+import com.zighang.jobposting.exception.JobPostingErrorCode
+import com.zighang.jobposting.infrastructure.mapper.CompanyMapper
 import com.zighang.jobposting.infrastructure.producer.JobAnalysisEventProducer
+import com.zighang.member.entity.value.Region
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 
@@ -16,7 +22,8 @@ class JobPostingService(
 //    private val analysisCaller: JobAnalysisCaller,
 //    private val cardJobPosingAnalysisDtoMapper: CardJobPosingAnalysisDtoMapper,
     private val cardService: CardService,
-    private val jobAnalysisEventProducer: JobAnalysisEventProducer
+    private val jobAnalysisEventProducer: JobAnalysisEventProducer,
+    private val companyMapper: CompanyMapper
 ) {
     fun top3ByJob(depthOne: String?, depthTwo: List<String>, memberId: Long): List<CardRedis> {
 
@@ -155,6 +162,57 @@ class JobPostingService(
                     jobPosting.teamInfo
                 ).joinToString("\n")
                     .ifBlank { "데이터 없음" }
+        }
+    }
+
+    fun getOneJobPosting(postingId: Long) : JobPostingDetailResponseDto {
+        val jobPosting = jobPostingRepository.findById(postingId)
+            .orElseThrow{ throw JobPostingErrorCode.NOT_EXISTS_JOB_POSTING.toException() }
+
+        val education = jobPosting.education.displayName
+        val depthTwo = jobPosting.depthTwo
+        val career = getCareer(jobPosting)
+        val workType = getWorkType(jobPosting)
+        val region = getRegion(jobPosting)
+
+        // 이미지 url 들어가는 부분 조정하기 -> cloudfrontUrl
+        val company = companyMapper.toJsonDto(jobPosting.company)
+
+
+        return JobPostingDetailResponseDto(
+            education = education,
+            depthTwo = depthTwo,
+            career = career,
+            workType = workType,
+            region = region,
+        )
+    }
+
+    private fun getWorkType(jobPosting: JobPosting): String {
+        val workTypes = jobPosting.recruitmentType.split(",")
+            .mapNotNull { RecruitmentType.entries.find { enumVal -> enumVal.name == it } }
+
+        return workTypes.joinToString(", ") {
+            it.displayName
+        }
+    }
+
+    private fun getRegion(jobPosting: JobPosting) : String {
+        val regions = jobPosting.recruitmentRegion.split(",")
+            .mapNotNull { Region.entries.find { enumVal -> enumVal.name == it } }
+
+        return regions.joinToString (", "){
+            it.regionName
+        }
+    }
+
+    private fun getCareer(jobPosting: JobPosting) : String {
+        return when {
+            jobPosting.minCareer == -1 && jobPosting.maxCareer == -1 -> "무관"
+            jobPosting.minCareer == 0 && jobPosting.maxCareer == 0 -> "신입"
+            jobPosting.minCareer >= 3 && jobPosting.maxCareer <= 3 -> "경력 3년 이상"
+            jobPosting.minCareer > 0 && jobPosting.maxCareer > 0 -> "경력 ${jobPosting.minCareer}-${jobPosting.maxCareer}년"
+            else -> "기타"
         }
     }
 }
