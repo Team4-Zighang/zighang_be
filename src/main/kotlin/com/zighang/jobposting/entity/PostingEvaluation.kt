a37f1a76dd666d2f82c3bea9529240775a30c09c
chyun7114 <chyun7114@naver.com>
2025-09-09 14:38:39 +0900

    Feat: 공고평 저장 table 생성

---
+++ src/main/kotlin/com/zighang/jobposting/entity/PostingEvaluation.kt
@@ -0,0 +1,30 @@
+package com.zighang.jobposting.entity
+
+import com.zighang.jobposting.entity.value.RecruitmentStep
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "posting_evaluation")
+class PostingEvaluation (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(name = "member_id", nullable = false)
+    val memberId : Long,
+
+    @Column(name = "posting_id", nullable = false)
+    val postingId : Long,
+
+    @Column(name = "eval_score", nullable = false)
+    val evalScore : Int,
+
+    @Column(name = "eval_text", nullable = false)
+    val evalText : String,
+
+    @Column(name = "recruitment_step", nullable = false)
+    @Enumerated(EnumType.STRING)
+    val recruitmentStep : RecruitmentStep,
+){
+}
