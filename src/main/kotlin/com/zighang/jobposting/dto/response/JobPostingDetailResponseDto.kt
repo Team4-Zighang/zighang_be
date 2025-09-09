a9573d3afbfe04f7f17043b4bfa2188e8475e006
chyun7114 <chyun7114@naver.com>
2025-09-09 22:07:44 +0900

    Feat: 1차 공고 dto 변수 설정 완료

---
+++ src/main/kotlin/com/zighang/jobposting/dto/response/JobPostingDetailResponseDto.kt
@@ -0,0 +1,11 @@
+package com.zighang.jobposting.dto.response
+
+data class JobPostingDetailResponseDto(
+    val education : String,
+    val depthTwo : String,
+    val career : String,
+    val workType : String,
+    val region : String,
+) {
+
+}
