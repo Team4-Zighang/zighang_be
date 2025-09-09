b8beb893f24781454de85e3f498de4e346573626
chyun7114 <chyun7114@naver.com>
2025-09-09 21:24:45 +0900

    Fix: content 칼럼 타입 전환 및 이벤트 발행 조건 수정
    # Conflicts:
    #	src/main/kotlin/com/zighang/jobposting/service/JobPostingService.kt

--- src/main/kotlin/com/zighang/jobposting/service/JobPostingService.kt
+++ src/main/kotlin/com/zighang/jobposting/service/JobPostingService.kt
@@ -19,6 +19,8 @@ import java.time.LocalDateTime
 @Service
 class JobPostingService(
     private val jobPostingRepository: JobPostingRepository,
+//    private val analysisCaller: JobAnalysisCaller,
+//    private val cardJobPosingAnalysisDtoMapper: CardJobPosingAnalysisDtoMapper,
     private val cardService: CardService,
     private val jobAnalysisEventProducer: JobAnalysisEventProducer,
 ) {
